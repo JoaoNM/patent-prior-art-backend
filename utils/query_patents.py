071127e537b3fa526699bf76992530913c375617
Joao Lucas NM <51080629+JoaoNM@users.noreply.github.com>
2023-03-25 18:24:31 +0800

    Restructure

--- utils/query_patents.py
+++ utils/query_patents.py
@@ -9,20 +9,22 @@ project_id = 'plasma-hope-381607'
 bq_client = bigquery.Client(project=project_id)
 
 def find_similar_patents_to(keyword):
-    # Returns Top 10 patents with the keyword
+
+    print(f"Searching BQ for top 25 patents related to {keyword}...")
+
     query = r"""
     SELECT publication_number, abstract, url
     FROM `patents-public-data.google_patents_research.publications` 
     WHERE abstract LIKE '%{}%'
-    LIMIT 50
+    LIMIT 25
     """.format(keyword)
 
     df = bq_client.query(query).to_dataframe()
 
-    print("success!")
-    print(df.head(50))
+    print("Success! Top 5 patents:")
+    print(df.head())
 
     return df
 
 # test case: 
-find_similar_patents_to("machine learning")
+# find_similar_patents_to("machine learning")
