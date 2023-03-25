d308628ca93baf9dc0180aca453c30415e1d4ac5
Joao Lucas NM <51080629+JoaoNM@users.noreply.github.com>
2023-03-25 17:35:02 +0800

    Add GPT-3.5-turbo API query

---
+++ utils/query_gpt.py
@@ -0,0 +1,39 @@
+import OPENAI_KEY
+import openai
+
+API_KEY = OPENAI_KEY.OPENAI_API_KEY
+
+openai.api_key = API_KEY
+openai.organization = "org-hzFOUxcvM8LcAPohvF4WAIYY"
+
+
+# Below is sample code to play around for fun. 
+
+user_query = ""
+
+chat = [
+    {"role": "user", "content": "Hello! What's the weather like in Johor Bahru? I'm planning a trip there."},
+]
+
+while (user_query != "exit"):   
+
+    if (user_query == "show chat"):
+        print(chat)
+        user_query = input("User: ")
+        continue
+
+    sample_request = {
+        "model": "gpt-3.5-turbo",
+        "messages": chat,
+    }
+
+    response = openai.ChatCompletion.create(**sample_request)
+
+    print(response.choices[0].message)
+
+    user_query = input("User: ")
+
+    chat.append(response.choices[0].message)
+    chat.append({"role": "user", "content": user_query})
+
+print("Thanks for chatting!")
