b4a7efc7e1112503f128f0064e02898f88e2690c
Joao Lucas NM <51080629+JoaoNM@users.noreply.github.com>
2023-03-26 10:34:55 +0800

    Adding concurrent futures for local model usage
    Testing concurrent futures. Processed 8 requests in 11 seconds.

--- keyword_inference/local_fill_mask.py
+++ keyword_inference/local_fill_mask.py
@@ -1,4 +1,6 @@
 from transformers import AutoModelForMaskedLM, AutoTokenizer, pipeline
+import concurrent.futures
+import multiprocessing as mp
 
 model_local_path = "./bert-for-patents"
 
@@ -10,19 +12,47 @@ fill_mask_pipeline = pipeline("fill-mask", model=model, tokenizer=tokenizer)
 
 # write a function that replaces a given word with '[MASK]' and returns the result
 def find_related_keywords(input_text, word):
-    results = [[], [], [], [], []]
-    j = 0
     input_text = input_text.replace(word, '[MASK]')
     result = fill_mask_pipeline(input_text)
-    for i in result: 
-        results[j] = [i['token_str'], i['score']]
-        j += 1
-    return results
+    
+    return result
 
-input_text = "The present [MASK] provides a torque sensor that is small and highly rigid and for which high production efficiency is possible."
+if __name__ == '__main__':
 
-result = fill_mask_pipeline(input_text)
+    mp.set_start_method('spawn')
 
-for i in result:
-    print(i['token_str'], i['score'])
-    print('\n')
+    input_texts = [
+        "The present innovation provides a torque sensor that is small and highly rigid and for which high production efficiency is possible.",
+        "The present innovation provides a torque sensor that is small and highly rigid and for which high production efficiency is possible.",
+        "The present innovation provides a torque sensor that is small and highly rigid and for which high production efficiency is possible.",
+        "The present innovation provides a torque sensor that is small and highly rigid and for which high production efficiency is possible.",
+        "The present innovation provides a torque sensor that is small and highly rigid and for which high production efficiency is possible.",
+        "The present innovation provides a torque sensor that is small and highly rigid and for which high production efficiency is possible.",
+        "The present innovation provides a torque sensor that is small and highly rigid and for which high production efficiency is possible.",
+        "The present innovation provides a torque sensor that is small and highly rigid and for which high production efficiency is possible.",
+    ]
+
+    mask_words = [
+        "innovation",
+        "torque",
+        "sensor",
+        "production",
+        "efficiency",
+        "rigid",
+        "possible",
+        "highly",
+    ]
+
+    # Use ProcessPoolExecutor to parallelize the pipeline calls
+    with concurrent.futures.ProcessPoolExecutor(max_workers=1) as executor:
+        # Run the pipeline on each input text in parallel
+        results = list(executor.map(find_related_keywords, input_texts, mask_words))
+
+    print(results)
+
+
+# single_input_text = "The present innovation provides a torque sensor that is small and highly rigid and for which high production efficiency is possible."
+
+# result = find_related_keywords(single_input_text, "innovation")
+
+# print(result)
